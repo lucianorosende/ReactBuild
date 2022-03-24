d1916112643dd2a17a837df7285995705d20e0dc
zickz4g <lucianorosende@gmail.com>
2022-03-24 11:51:41 -0300

    Test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Proximamente una App de Luciano
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprender React
         </a>
       </header>
     </div>
