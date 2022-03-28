4193bf9908f1225a7eb73f8776d78e8b2025d415
zickz4g <lucianorosende@gmail.com>
2022-03-28 17:59:46 -0300

    page added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/navBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Proximamente una App de Luciano
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprender React
-        </a>
-      </header>
-    </div>
-  );
+    <NavBar/>
+    
+  )
+    
+  
 }
 
 export default App;
