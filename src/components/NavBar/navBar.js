ebf92b50200f14f0fdab0da8ff5e128903db4fa1
zickz4g <lucianorosende@gmail.com>
2022-03-29 09:20:32 -0300

    npm install

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -70,6 +70,7 @@ let nav = () => {
                 </section>
 
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>  
+
             </body>
         
         
