ff8dd09a38f45fcaf0383d4e66267466fcdf33c1
zickz4g <lucianorosende@gmail.com>
2022-03-29 13:14:36 -0300

    dropdown

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -1,5 +1,3 @@
-
-
 let nav = () => {
 
     return (
@@ -69,7 +67,7 @@ let nav = () => {
                     </div>
                 </section>
 
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>  
+         
 
             </body>
         
