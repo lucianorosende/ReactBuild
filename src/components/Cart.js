aa7c35e6fc638d17bccff27aaafe71418253cb5f
zickz4g <lucianorosende@gmail.com>
2022-05-13 02:37:21 -0300

    cart html and css

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -93,107 +93,85 @@ const Cart = () => {
         return (
             <>
 
-            <div className="cart_area section_padding_100 clearfix">
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <div className="cart-table clearfix">
-                            <table className="table table-responsive">
-                                <thead>
-                                    <tr>
-                                        <th>Product</th>
-                                        <th>Price</th>
-                                        <th>Quantity</th>
-                                        <th>Total</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td className="cart_product_img d-flex align-items-center">
-                                            <a href="#"><img src="" alt="Product"/></a>
-                                            <h6>Yellow Cocktail Dress</h6>
-                                        </td>
-                                        <td className="price"><span>$49.88</span></td>
-                                        <td className="qty">
-                                            <div className="quantity">
-                                                <span className="qty-minus"><i className="fa fa-minus" aria-hidden="true"></i></span>
-                                                <input type="number" className="qty-text" id="qty" step="1" min="1" max="99" name="quantity" value="1"/>
-                                                <span className="qty-plus"><i className="fa fa-plus" aria-hidden="true"></i></span>
-                                            </div>
-                                        </td>
-                                        <td className="total_price"><span>$49.88</span></td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
+            
 
-                        <div className="cart-footer d-flex mt-30">
-                            <div className="back-to-shop w-50">
-                                <a href="shop-grid-left-sidebar.html">Continue shooping</a>
-                            </div>
-                            <div className="update-checkout w-50 text-right">
-                                <a href="#">clear cart</a>
-                                <a href="#">Update cart</a>
-                            </div>
-                        </div>
 
-                    </div>
-                </div>
 
-                <div className="row">
-                    <div className="col-12 col-md-6 col-lg-4">
-                        <div className="coupon-code-area mt-70">
-                            <div className="cart-page-heading">
-                                <h5>Cupon code</h5>
-                                <p>Enter your cupone code</p>
-                            </div>
-                            <form action="#">
-                                <input type="search" name="search" placeholder="#569ab15"/>
-                                <button type="submit">Apply</button>
-                            </form>
-                        </div>
-                    </div>
-                    <div className="col-12 col-md-6 col-lg-4">
-                        <div className="shipping-method-area mt-70">
-                            <div className="cart-page-heading">
-                                <h5>Shipping method</h5>
-                                <p>Select the one you want</p>
-                            </div>
+<div className="shopping-cart">
+    <h1 className="d-flex justify-content-center">Mi carrito</h1>
+  <div className="column-labels">
+    <label className="product-image">Imagen</label>
+    <label className="product-details">Producto</label>
+    <label className="product-price">Precio</label>
+    <label className="product-quantity">Cantidad</label>
+    <label className="product-removal">Remover</label>
+    <label className="product-line-price">Total</label>
+  </div>
 
-                            <div className="custom-control custom-radio mb-30">
-                                <input type="radio" id="customRadio1" name="customRadio" className="custom-control-input"/>
-                                <label className="custom-control-label d-flex align-items-center justify-content-between" htmlFor="customRadio1"><span>Next day delivery</span><span>$4.99</span></label>
-                            </div>
+  <div className="product">
+    <div className="product-image">
+      <img src="https://s.cdpn.io/3/dingo-dog-bones.jpg"/>
+    </div>
+    <div className="product-details">
+      <div className="product-title">Dingo Dog Bones</div>
+      <p className="product-description">The best dog bones of all time. Holy crap. Your dog will be begging for these things! I got curious once and ate one myself. I'm a fan.</p>
+    </div>
+    <div className="product-price">12.99</div>
+    <div className="product-quantity">
+      <input type="number" value="2" min="1"/>
+    </div>
+    <div className="product-removal">
+      <button className="remove-product">
+        Remove
+      </button>
+    </div>
+    <div className="product-line-price">25.98</div>
+  </div>
 
-                            <div className="custom-control custom-radio mb-30">
-                                <input type="radio" id="customRadio2" name="customRadio" className="custom-control-input"/>
-                                <label className="custom-control-label d-flex align-items-center justify-content-between" htmlFor="customRadio2"><span>Standard delivery</span><span>$1.99</span></label>
-                            </div>
+  <div className="product">
+    <div className="product-image">
+      <img src="https://s.cdpn.io/3/large-NutroNaturalChoiceAdultLambMealandRiceDryDogFood.png"/>
+    </div>
+    <div className="product-details">
+      <div className="product-title">Nutro™ Adult Lamb and Rice Dog Food</div>
+      <p className="product-description">Who doesn't like lamb and rice? We've all hit the halal cart at 3am while quasi-blackout after a night of binge drinking in Manhattan. Now it's your dog's turn!</p>
+    </div>
+    <div className="product-price">45.99</div>
+    <div className="product-quantity">
+      <input type="number" value="1" min="1"/>
+    </div>
+    <div className="product-removal">
+      <button className="remove-product">
+        Remove
+      </button>
+    </div>
+    <div className="product-line-price">45.99</div>
+  </div>
 
-                            <div className="custom-control custom-radio">
-                                <input type="radio" id="customRadio3" name="customRadio" className="custom-control-input"/>
-                                <label className="custom-control-label d-flex align-items-center justify-content-between" for="customRadio3"><span>Personal Pickup</span><span>Free</span></label>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-12 col-lg-4">
-                        <div className="cart-total-area mt-70">
-                            <div className="cart-page-heading">
-                                <h5>Cart total</h5>
-                                <p>Final info</p>
-                            </div>
+  <div className="totals">
+    <div className="totals-item">
+      <label>Subtotal</label>
+      <div className="totals-value" id="cart-subtotal">71.97</div>
+    </div>
+    <div className="totals-item">
+      <label>Tax (5%)</label>
+      <div className="totals-value" id="cart-tax">3.60</div>
+    </div>
+    <div className="totals-item">
+      <label>Shipping</label>
+      <div className="totals-value" id="cart-shipping">15.00</div>
+    </div>
+    <div className="totals-item totals-item-total">
+      <label>Grand Total</label>
+      <div className="totals-value" id="cart-total">90.57</div>
+    </div>
+  </div>
+      
+      <button className="checkout">Checkout</button>
 
-                            <ul className="cart-total-chart">
-                                <li><span>Subtotal</span> <span>$59.90</span></li>
-                                <li><span>Shipping</span> <span>Free</span></li>
-                                <li><span><strong>Total</strong></span> <span><strong>$59.90</strong></span></li>
-                            </ul>
-                            <a href="checkout.html" className="btn karl-checkout-btn">Proceed to checkout</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+</div>
+
+
 
 
             
